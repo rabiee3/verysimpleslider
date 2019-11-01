ebc1b7b16f5a12e318cf582411f34ab67072b6df
Rabie Mohamed <rabea.m@moto-soft.com>
2019-11-01 07:35:08 +0200

    added basic slider funtionality

--- projects/verysimpleslider/src/lib/verysimpleslider.component.ts
+++ projects/verysimpleslider/src/lib/verysimpleslider.component.ts
@@ -3,27 +3,44 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'very-simple-slider',
   templateUrl: './very-simple-slider.html',
-  styleUrls: ['./very-simple-slider.scss'],
+  styleUrls: ['./very-simple-slider.scss','./animate.css'],
 })
 export class VerysimplesliderComponent implements OnInit {
 
-  @Input() items = [];
+  @Input() items;
   private progressValue = 0;
   private progressInterval;
   private loopInterval;
-  private currentSlide = 0;
-  private slideDuration = 10000;
+  currentSlide = 0;
+  slideAnimName;
+  slideAnimOppName;
+  @Input() slideDuration = 100000;
+  @Input() animType;
+  @Input() animDirection;
+  constructor() { 
 
-  constructor() { }
+  }
 
   ngOnInit() {
+    if(this.animType=="slide"){
+      if(this.animDirection=="right"){
+        this.slideAnimName = "slideInLeft";
+        this.slideAnimOppName = "slideOutRight";
+      }else{
+        this.slideAnimName = "slideInRight";
+        this.slideAnimOppName = "slideOutLeft";
+      }
+    }else if(this.animType=="fade"){
+      this.slideAnimName = "fadeIn";
+      this.slideAnimOppName = "fadeOut";
+    }
     this.customSlider_init();
   }
 
     //------------start slider logic----------//
     customSlider_init(){
-      this.next();
       this.start();
+      this.next();
     }
   
     start() {
@@ -43,14 +60,47 @@ export class VerysimplesliderComponent implements OnInit {
   
     prev() {
       this.progressValue = 0;
+
+        //reverse slide anim
+        if(this.animDirection=="right"){
+          this.slideAnimName = "slideInRight";
+          this.slideAnimOppName = "slideOutLeft";
+        }else{
+          this.slideAnimName = "slideInLeft";
+          this.slideAnimOppName = "slideOutRight";
+        }
+  
         this.currentSlide -- ;
         if (this.currentSlide < 1) {
             this.currentSlide = this.items.length;
         }
+
+        //restart time
+        clearInterval(this.loopInterval);
+        this.loopInterval = setInterval(()=>{
+          //revert back to normal animation
+          if(this.animDirection=="right"){
+            this.slideAnimName = "slideInLeft";
+            this.slideAnimOppName = "slideOutRight";
+          }else{
+            this.slideAnimName = "slideInRight";
+            this.slideAnimOppName = "slideOutLeft";
+          }
+          this.next();
+        }, this.slideDuration);
+
+
     }
   
     next() {
       this.progressValue = 0;
+
+      //restart time
+      clearInterval(this.loopInterval);
+      this.loopInterval = setInterval(()=>{
+        this.next();
+      }, this.slideDuration);
+
         this.currentSlide ++ ;
         if (this.currentSlide > this.items.length) {
           this.currentSlide = 1;
